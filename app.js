8f76a0277af6722fe8abc5dee4dffcb17da46b9c
Gabriella C Maciel <gabriellacunhamaciel@gmail.com>
2025-09-04 18:03:16 -0300

    adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+   let InputAmigo = document.getElementById("amigo").value;
+
+    if (InputAmigo === "") {
+        alert("Por favor, insira o nome de um amigo.");
+        return;
+    }
+
+    amigos.push(amigo);
+    document.getElementById("amigo").value = "";
+}
