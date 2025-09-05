89d1a365a7bb85ae1c4ef5d76f52a2f147b43b54
Gabriella C Maciel <gabriellacunhamaciel@gmail.com>
2025-09-05 18:49:04 -0300

    Att adicionar amigos

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let amigos = [];
 
 function adicionarAmigo() {
-   let InputAmigo = document.getElementById("amigo").value;
+   let InputAmigo = document.getElementById("amigo");
+   const nome = InputAmigo.value.trim();
 
     if (InputAmigo === "") {
         alert("Por favor, insira o nome de um amigo.");
         return;
     }
 
-    amigos.push(amigo);
+    amigos.push(nome);
     document.getElementById("amigo").value = "";
+
+    exibirAmigos();
 }
